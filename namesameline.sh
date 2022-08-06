fd49fc1bba855deb8271f35eafdf37145ade8912
DevanshiGupta481 <66218163+DevanshiGupta481@users.noreply.github.com>
2022-08-06 21:59:31 +0530

    Add files via upload

---
+++ namesameline.sh
@@ -0,0 +1,9 @@
+#                             Online Bash Shell.
+#                 Code, Compile, Run and Debug Bash script online.
+# Write your code in this editor and press "Run" button to execute it.
+
+
+Firstname="Devanshi"
+Lastname="Gupta"
+echo "$Firstname $Lastname"
+
